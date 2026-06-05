6d12f9a36452b5ffc3052b9ffacd5a993a2beccd
Nehal Mangat <nehalsinghmangat.software@gmail.com>
2026-06-05 12:29:21 -0700

    Clean up repo and data pipeline

--- paper/auto/predicting_drosophila_body_orientation_from_a_translational_trajectory_using_an_artificial_neural_network.el
+++ paper/auto/predicting_drosophila_body_orientation_from_a_translational_trajectory_using_an_artificial_neural_network.el
@@ -9,6 +9,13 @@
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -22,7 +29,9 @@
     "bm"
     "natbib")
    (LaTeX-add-labels
-    "eq:heading_correction_opt")
+    "eq:heading_correction_opt"
+    "fig:data_pipeline"
+    "fig:model_performance")
    (LaTeX-add-bibliographies
     "main"))
  :latex)
